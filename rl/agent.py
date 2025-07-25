d8f4896c964d59a570d0f2baade8f32ba1aeea75
Marcus <greenlittleapple@gmail.com>
2025-07-25 16:50:52 -0700

    Fix SB3 wrapper usage

--- rl/agent.py
+++ rl/agent.py
@@ -16,7 +16,9 @@ class PPOAgent:
         # Convert AECEnv -> ParallelEnv -> VectorEnv for SB3
         parallel_env = conversions.aec_to_parallel(env)
         vec_env = ss.pettingzoo_env_to_vec_env_v1(parallel_env)
-        self.vec_env = ss.stable_baselines3_vec_env_v0(vec_env, num_envs=1)
+        self.vec_env = ss.concat_vec_envs_v1(
+            vec_env, 1, num_cpus=1, base_class="stable_baselines3"
+        )
         self.model = PPO("MlpPolicy", self.vec_env, verbose=verbose)
 
     def train(self, timesteps: int = 1_000) -> None:
